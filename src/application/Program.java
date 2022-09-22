8c14becdd91c1a051cbc6f130df82f81a831324e
Gilberto Junior <gagjunior@hotmail.com>
2022-09-22 00:00:24 -0300

    Criar metodos para converter produto e escrever arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,43 +1,30 @@
 package application;
 
-import model.entities.Product;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.File;
 import java.util.List;
 import java.util.Scanner;
+
+import model.entities.Product;
 
 public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        List<Product> products = new ArrayList<>();
-
+        
         System.out.print("Digite o caminho do arquivo de entrada: ");
-        String path = sc.nextLine();
+        String strPath = sc.nextLine();
+        
+        List<Product> products = Product.productsFileConvert(strPath);
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+        File path = new File(strPath);
+        String originalDir = path.getParent();
+        boolean success = new File(originalDir + "//out").mkdir();
 
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                String[] dataLine = line.split(",");
-                String name = dataLine[0];
-                double amount = Double.parseDouble(dataLine[1]);
-                double quantity = Double.parseDouble(dataLine[2]);
-                products.add(new Product(name, amount, quantity));
-                line = bufferedReader.readLine();
-            }
+        System.out.println("Diretorio criado? " + success);
 
-            for (Product product : products) {
-                System.out.println(product);
-            }
+        String strSubDir = originalDir + "//out//summary.csv";
 
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+        Product.productsFileWrite(strSubDir, products);
 
         sc.close();
     }
--- src/model/entities/Product.java
+++ src/model/entities/Product.java
@@ -1,5 +1,13 @@
 package model.entities;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
     private String name;
     private double amount;
@@ -37,6 +45,41 @@ public class Product {
 
     public double totalAmount(){
         return amount * quantity;
+    }
+
+    public static List<Product> productsFileConvert(String strPath){
+        List<Product> products = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(strPath))) {
+            
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] dataLine = line.split(",");
+                String name = dataLine[0];
+                double amount = Double.parseDouble(dataLine[1]);
+                double quantity = Double.parseDouble(dataLine[2]);
+                products.add(new Product(name, amount, quantity));
+                line = bufferedReader.readLine();
+            }
+
+            return products;
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return products = null;
+        }
+        
+    }
+
+    public static void productsFileWrite(String strSubDir, List<Product> products){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(strSubDir))) {
+            for (Product product : products) {
+                bw.write(product.getName() + ", " + product.totalAmount());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
